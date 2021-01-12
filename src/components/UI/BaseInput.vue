9224be5b78e72ca0d81a62d4fdfd9a5483fb5556
Nikolaj Schmidt Christoffersen <niko7185@edu.campusvejle.dk>
2021-01-12 10:48:19 +0100

    Added an input component

---
+++ src/components/UI/BaseInput.vue
@@ -0,0 +1,44 @@
+
+<template>
+    <label :for="name">{{ label }}</label>
+    <input :type="inputType" :placeholder="placeholder" :id="name" v-model.trim="inputValue"/>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            default: "Input tekst herunder",
+        },
+        placeholder: {
+            type: String,
+            default: "Input tekst her",
+        },
+        inputType: {
+            type: String,
+            default: "text",
+        }
+    },
+    emits: ["value-change"],
+    data() {
+        return {
+            inputValue: null,
+        };
+    },
+    watch: {
+        inputValue(newValue) {
+
+            this.$emit("value-change", newValue, this.name);
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import BaseInput from './components/UI/BaseInput.vue'
+
 const app = createApp(App);
+
+app.component("base-input", BaseInput);
 
 app.use(router)
 
