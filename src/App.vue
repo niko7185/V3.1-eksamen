23d806627b8ba46953dc50db135f0b54ce587602
Nikolaj Schmidt Christoffersen <niko7185@edu.campusvejle.dk>
2021-01-12 09:39:07 +0100

    Started on the header and installed tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <TheHeader />
   <router-view/>
 </template>
+
+<script>
+import TheHeader from './components/navigation/TheHeader.vue'
+
+export default {
+  components: {
+    TheHeader,
+  },
+}
+</script>
 
 <style>
 #app {
